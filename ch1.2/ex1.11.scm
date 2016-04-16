43aaac50ed2ef540a4988377a6d1431aa53f3684
Mikko Suniala <mikko.suniala@iki.fi>
2016-04-16 23:29:18 +0300

    Exercise 1.11: Finished iterative implementation

--- ch1.2/ex1.11.scm
+++ ch1.2/ex1.11.scm
@@ -9,10 +9,9 @@
   (newline))
 
 (define (show-range label function start max)
-  (if (<= start max)
-      ((show label function start)
-       (show-range label function (+ start 1) max))
-      0))
+  (show label function start)
+  (if (< start max)
+      (show-range label function (+ start 1) max)))
 
 (display "f(n) = n if n<3 and f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n>= 3")
 (newline)
@@ -23,11 +22,14 @@
       (+ (frec (- n 1)) (* 2 (frec (- n 2))) (* 3 (frec (- n 3))))
   ))
 
-(show-range "frec" frec 1 10)
-
-
 (define (fiter n)
+  (define (fn-iter n max fn-3 fn-2 fn-1)
+    (define fn (+ fn-1 (* 2 fn-2) (* 3 fn-3)))
+    (cond ((< n max) (fn-iter (+ n 1) max fn-2 fn-1 fn))
+          (else fn)))
   (if (< n 3)
       n
-      3)
-  )
+      (fn-iter 3 n 0 1 2)))
+
+(show-range "frec" frec 1 25)
+(show-range "fiter" fiter 1 25)
